ac80c89897d0b4f3d40c40c8e5e322c40a407f03
satya190597 <satyaprakashnandy@gmail.com>
2021-06-15 21:57:42 +0530

    Format Date in month, day and year format

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,22 @@
 // + CSS Imports.
 import "./ExpenseItem.css";
 
-function ExpenseItem(props) {  
+function ExpenseItem(props) {
+  // + Convert standard date to month day and year format.
+  const months = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "numeric" });
+  const year = props.date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div>{props.date.toDateString()}</div>
+      <div>
+        <div>{months}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <p className="expense-item__price">{props.price}</p>
+        <p className="expense-item__price">{props.amount}</p>
       </div>
     </div>
   );
