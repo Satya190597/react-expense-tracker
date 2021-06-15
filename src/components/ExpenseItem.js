8fa7b2f9b7e2ed2fb3cbe44cf6bd3564e5097ab8
satya190597 <satyaprakashnandy@gmail.com>
2021-06-15 21:42:44 +0530

    Introduce props to the component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,24 +1,16 @@
 // + CSS Imports.
-import './ExpenseItem.css'
+import "./ExpenseItem.css";
 
-function ExpenseItem() {
-
-    // + Expense Data. 
-    const expenseDate = new Date()
-    const expenseDetail = "Car Insurance" 
-    const expensePrice = 28900
-
-    return (
-        <div className="expense-item">
-            <div>
-                {expenseDate.toDateString()}
-            </div>
-            <div className="expense-item__description">
-                <h2>{expenseDetail}</h2>
-                <p className="expense-item__price">{expensePrice}</p>
-            </div>
-        </div>
-    )
+function ExpenseItem(props) {  
+  return (
+    <div className="expense-item">
+      <div>{props.date.toDateString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <p className="expense-item__price">{props.price}</p>
+      </div>
+    </div>
+  );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
