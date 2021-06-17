456c7a4cebe45fb5f2ccb4f6507ec9cacf6e3b27
satya190597 <satyaprakashnandy@gmail.com>
2021-06-17 08:10:18 +0530

    Add temporary code to learn about - useState hook in react.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,36 @@
 // + CSS Imports.
 import "./ExpenseItem.css";
 
+// + Default Imports.
+import React, { useState } from "react";
+
 // + Component Imports.
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
+  // 1. Initialize title state.
+  const [title, setTitle] = useState(props.title);
+
+  // 2. Update title - click handler.
+  const updateTitleHandler = () => {
+    setTitle("Updated");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        {
+          // Title - OLD CODE
+          // <h2>{props.title}</h2>
+        }
+        <h2>{title}</h2>
         <p className="expense-item__price">{props.amount}</p>
+        {
+          // Test Objective - Learn state management in react.
+        }
+        <button onClick={updateTitleHandler}>Update Title</button>
       </div>
     </Card>
   );
