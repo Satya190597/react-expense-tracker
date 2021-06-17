027822d66fec6a96c7fab8542041927e9bb75f76
satya190597 <satyaprakashnandy@gmail.com>
2021-06-17 10:53:11 +0530

    Add input change handler in ExpenseForm component

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -5,22 +5,62 @@
 // + Import CSS
 import "./ExpenseForm.css";
 
+// + Default Imports
+import React, { useState } from "react";
+
 function ExpenseForm() {
+  // Initialize expense data.
+  const [expenseData, setExpenseData] = useState({
+    expenseTitle: "",
+    expenseAmount: "",
+    expenseDate: "",
+  });
+
+  // Input Handler - Set expense data from user input.
+  const inputChangeHandler = (event) => {
+    setExpenseData((previousState) => {
+      return {
+        ...previousState,
+        // we need square brackets to tell that this refers to dynamic key name.
+        [event.target.name]: event.target.value,
+      };
+    });
+  };
+
   return (
     <form className="new-expense__controls">
       <div className="new-expense__control">
         <label>Title</label>
-        <input type="text" />
+        <input
+          type="text"
+          value={expenseData.expenseTitle}
+          name="expenseTitle"
+          onChange={inputChangeHandler}
+        />
       </div>
       <div className="new-expense__control">
         <label>Amount</label>
-        <input type="number" min="0" step="0.1" />
+        <input
+          type="number"
+          min="0"
+          step="0.1"
+          value={expenseData.expenseAmount}
+          name="expenseAmount"
+          onChange={inputChangeHandler}
+        />
       </div>
       <div className="new-expense__control">
         <label>Date</label>
-        <input type="date" min="2020-01-01" max="2050-01-01"/>
+        <input
+          type="date"
+          min="2020-01-01"
+          max="2050-01-01"
+          value={expenseData.expenseDate}
+          name="expenseDate"
+          onChange={inputChangeHandler}
+        />
       </div>
-      <div>
+      <div className="new-expense__actions">
         <button>Add</button>
       </div>
     </form>
